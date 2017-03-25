060628a8c1a01c695e11b43fa2724292418aa491
Luis Carlos Isaula <luisca_elfather@hotmail.com>
2017-03-24 19:28:21 -0600

    Autofac testing by @Raimmaster

--- CSVConverter/Program.cs
+++ CSVConverter/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Autofac;
+using CSVConverterLogic;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,8 @@ namespace CSVConverter
 {
     class Program
     {
+        private static IContainer Container { get; set; }
+
         static void Main(string[] args)
         {
             if(args.Length > 2)
@@ -16,6 +21,28 @@ namespace CSVConverter
                 var conversionType = args[1];
                 var outputPath = args[2];
 
+                var builder = new ContainerBuilder();
+                //builder.RegisterType<ConsoleOutput>().As<IOutput>();
+                //builder.RegisterType<TodayWriter>().As<IDateWriter>();
+
+                builder.RegisterType<FileParser>().As<IFileParser>();
+                builder.RegisterType<StreamWriter>().As<TextWriter>();
+                builder.RegisterType<XMLBuilder>().As<ICsvConverter>();
+
+                Container = builder.Build();
+
+                BuildFileFromCSV();
+            }
+        }
+
+        public static void BuildFileFromCSV()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var converter = scope.Resolve<CSVConverterLogic.CSVConverter>();
+
+                var csvParsedObject = converter.ParseFile();
+                converter.WriteConvertedCSV(converter.MakeObject(csvParsedObject));
 
             }
         }
